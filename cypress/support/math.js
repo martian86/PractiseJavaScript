a5000becd1de19b66b990de0ca98b48b8e498645
ani486 <aniruddha.parlikar@appdirect.com>
2021-06-12 13:03:06 -0400

    adding functions

---
+++ cypress/support/math.js
@@ -0,0 +1,8 @@
+module.exports = {
+    square: (x) =>{
+    return x*x;
+    },
+        sum: (a,b)=>{
+           return a+b;
+          }
+}
---
+++ cypress/support/strings.js
@@ -0,0 +1,4 @@
+let firstName = "John";
+let lastName = "Cena"
+
+exports.stringsFirstName=firstName;
